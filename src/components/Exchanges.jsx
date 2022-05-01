3f9eef1404641889f30bf282803fb8a863752e03
ud17 <upandya1297@gmail.com>
2022-05-01 17:21:24 -0400

    v.1.0: bug fix

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -3,16 +3,15 @@ import millify from 'millify';
 import { Collapse, Row, Col, Typography, Avatar } from 'antd';
 import HTMLReactParser from 'html-react-parser';
 
-import { useGetExchangesQuery } from '../services/cryptoApi';
-import Loader from './Loader';
+// import { useGetExchangesQuery } from '../services/cryptoApi';
 
 const { Text } = Typography;
 const { Panel } = Collapse;
 
 const Exchanges = () => {
-  const { data, isFetching } = useGetExchangesQuery();
+  const { data, isFetching } = "";//useGetExchangesQuery();
   const exchangesList = data?.data?.exchanges;
-  if (isFetching) return <Loader />;
+  if (isFetching) return "Loading...";
 
   return (
     <>
